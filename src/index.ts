379a8e136efa5e257541987cb2ad3aab6f365b1d
asus <casadadaista@gmail.com>
2022-11-10 12:00:14 -0300

    first working changes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,20 @@
-console.log('hello world');
+// console.log('hello world');
 
-let age: number = 20;
+// let age: number = 20;
+
+// if (age < 50)
+//     age += 10
+// console.log(age)
 
 
+let sales = 123_456_789
+let course = 'Typescript'
+let is_published = true
+let level
+let whatchamacallit = 123090
 
+whatchamacallit = 10249809
+
+function render(document) {
+    console.log(document)
+}
